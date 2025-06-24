e402b3df2964d81dfef195b9211d9cb608b7d68e
Calle20 <97348165+Calle20@users.noreply.github.com>
2025-06-24 11:42:39 +0200

    zoom to country on loading works

--- MapPage.xaml.cs
+++ MapPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using BruTile.Predefined;
 using BruTile.Web;
+using BruTile.Wms;
 using Mapsui;
 using Mapsui.Limiting;
 using Mapsui.Projections;
@@ -9,6 +10,7 @@ using Mapsui.Tiling.Layers;
 using Mapsui.UI;
 using Mapsui.UI.Maui;
 using Mapsui.Utilities;
+using Microsoft.Maui.ApplicationModel;
 using System.Globalization;
 using Map = Mapsui.Map;
 
@@ -17,14 +19,15 @@ namespace TravelTracker
     public partial class MapPage : ContentPage
     {
         readonly MapControl _mapControl = new MapControl();
-        private static readonly Dictionary<string, MRect> CountryEnvelopes = new()
+        static readonly Dictionary<string, MRect> CountryEnvelopes = new Dictionary<string, MRect>
         {
-            // Germany (approx)
-            ["DE"] = new MRect(561000, 5930000, 1300000, 7000000),
-            // France (approx)
-            ["FR"] = new MRect(-6000000, 450000, -200000, 7000000),
-            // … add your other countries …
+            { "DE", new MRect(667510, 5984026, 1670585, 7269661) },   // Germany
+            { "FR", new MRect(-556597, 5181236, 1064703, 6665628) },  // France
+            { "US", new MRect(-13957016, 2881529, -7453304, 6449785) }, // USA
+            { "GB", new MRect(-842586, 6446275, 187058, 7820567) },   // United Kingdom
+            { "IN", new MRect(7594069, 887586, 10886340, 4213004) },
         };
+
 
         public MapPage()
         {
@@ -43,7 +46,7 @@ namespace TravelTracker
             // OSM base layer
             var osmLayer = OpenStreetMap.CreateTileLayer();
             osmLayer.Name = "OSM Base";
-            osmLayer.Opacity = 0.5;
+            osmLayer.Opacity = 1;
             map.Layers.Add(osmLayer);
 
             map.Navigator.Limiter = new ViewportLimiterKeepWithinExtent();
@@ -53,10 +56,14 @@ namespace TravelTracker
 
             // 4) Zoom to user's country or world
             var countryCode = new RegionInfo(CultureInfo.CurrentCulture.Name).TwoLetterISORegionName.ToString();
-            Console.WriteLine($"Detected country code: {countryCode}");
-            Console.WriteLine($"Country envelope: {CountryEnvelopes.ContainsKey(countryCode)}");
             if (CountryEnvelopes.TryGetValue(countryCode, out var bbox))
-                map.Navigator.ZoomToBox(bbox, MBoxFit.Fit);
+            {
+                _mapControl.SizeChanged += (_, __) =>
+                {
+                    _mapControl.Map.Navigator.ZoomToBox(bbox, MBoxFit.Fit);
+                };
+                
+            }
             else
                 map.Navigator.ZoomToBox(new MRect(-20_037_508, -20_037_508, 20_037_508, 20_037_508), MBoxFit.Fit);
 
